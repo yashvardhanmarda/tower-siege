13e596b5f58054f42805e5344d65e9768c273bf2
yashvardhanmarda <65061668+yashvardhanmarda@users.noreply.github.com>
2021-03-31 17:57:52 +0530

    Add files via upload

---
+++ PROJECT-29--master/PROJECT-29--master/Block.js
@@ -0,0 +1,36 @@
+class Block{
+    constructor(x, y, width, height, angle) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.visibility=255;
+
+        World.add(world, this.body);
+      }
+
+      score(){
+        if(this.visibility<0 && this.visibility>-50){
+          score++
+        }
+      }
+      display(){
+      if(this.body.speed<3){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+       rect(0, 0, this.width, this.height);
+        pop();
+      }
+        else{
+          World.remove(world,this.body)
+          this.visibility=this.visibility-10
+        }
+      }
+}
